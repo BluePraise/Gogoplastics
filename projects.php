cc85d058a76b994b7d5f7101777b8733fbccd349
MagalieLindaChetrit <magaliechetrit@gmail.com>
2020-02-21 13:12:49 +0100

    project title styling

--- projects.php
+++ projects.php
@@ -1,11 +1,25 @@
 <?php
 
-/* Template Name: Projects Template */
-
+/*  Template Name: Projects Template
+*/
 get_header();
 ?>
 
 <main id="site-content" role="main">
+
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+        <h2 class="page-title"><?php the_title(); ?></h2>
+
+        <?php // If comments are open or we have at least one comment, load up the comment template.
+        if ( comments_open() || get_comments_number() ) :
+            comments_template();
+        endif;
+
+        endwhile; // End of the loop.?>
+
+
 
 </main>
 <?php get_footer(); ?>
