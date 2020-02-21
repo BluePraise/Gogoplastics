a45a522698d6da97d1652047d38a9daee9fef042
MagalieLindaChetrit <magaliechetrit@gmail.com>
2020-02-21 12:51:02 +0100

    added custom post type for projects

--- functions.php
+++ functions.php
@@ -97,6 +97,54 @@
     }
 
 
+    // 06 Custom Posttypes for projects
+    function projects_posttype() {
+
+        $labels = array(
+            'name'                  => _x( 'Projecten', 'Post type general name', 'textdomain' ),
+            'singular_name'         => _x( 'Project', 'Post type singular name', 'textdomain' ),
+            'menu_name'             => _x( 'Projecten', 'Admin Menu text', 'textdomain' ),
+            'name_admin_bar'        => _x( 'Project', 'Add New on Toolbar', 'textdomain' ),
+            'add_new'               => __( 'Add New', 'textdomain' ),
+            'add_new_item'          => __( 'Add New Project', 'textdomain' ),
+            'new_item'              => __( 'New Project', 'textdomain' ),
+            'edit_item'             => __( 'Edit project', 'textdomain' ),
+            'view_item'             => __( 'View project', 'textdomain' ),
+            'all_items'             => __( 'All projects', 'textdomain' ),
+            'search_items'          => __( 'Search projects', 'textdomain' ),
+            'parent_item_colon'     => __( 'Parent projects:', 'textdomain' ),
+            'not_found'             => __( 'No projects found.', 'textdomain' ),
+            'not_found_in_trash'    => __( 'No projects found in Trash.', 'textdomain' ),
+            'featured_image'        => _x( 'project Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+            'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+            'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+            'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+            'archives'              => _x( 'project archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+            'insert_into_item'      => _x( 'Insert into project', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+            'uploaded_to_this_item' => _x( 'Uploaded to this project', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+            'filter_items_list'     => _x( 'Filter projects list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+            'items_list_navigation' => _x( 'projects list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+            'items_list'            => _x( 'projects list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
+        );
+
+        $args = array(
+            'labels'             => $labels,
+            'public'             => true,
+            'publicly_queryable' => true,
+            'show_ui'            => true,
+            'show_in_menu'       => true,
+            'query_var'          => true,
+            'rewrite'            => array( 'slug' => 'project' ),
+            'capability_type'    => 'post',
+            'has_archive'        => true,
+            'hierarchical'       => false,
+            'menu_position'      => null,
+            'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+        );
+
+        register_post_type( 'projects', $args );
+    }
+
     // Add Actions
 
     // 01 custom logo functionality
@@ -113,6 +161,9 @@
 
     // 05 Add Menu
     add_action( 'init', 'register_gogo_menu' );
+
+    // 06 hook for custom posttypes
+    add_action( 'init', 'projects_posttype' );
 
 
     // Remove Actions
---
+++ projects.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Template Name: Projects Template */
+
+get_header();
+?>
+
+<main id="site-content" role="main">
+
+</main>
+<?php get_footer(); ?>
