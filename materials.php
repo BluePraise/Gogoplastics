1f780115cd6894a693baba5cefcab8a3c4bacdf9
MagalieLindaChetrit <magaliechetrit@gmail.com>
2020-02-23 13:47:13 +0100

    content for materials page in ACF

--- materials.php
+++ materials.php
@@ -48,31 +48,42 @@ get_header();
                     <?php
                         $gallery_three_images    = get_field('gallery_three_images');
                         $image_one               = $gallery_three_images['image_one'];
+                        $image_one_caption       = $gallery_three_images['image_one_caption'];
                         $image_two               = $gallery_three_images['image_two'];
+                        $image_two_caption       = $gallery_three_images['image_two_caption'];
                         $image_three             = $gallery_three_images['image_three'];
+                        $image_three_caption     = $gallery_three_images['image_three_caption'];
                     ?>
 
-                    <figure>
+                    <figure class="image--one">
                         <img src="<?php echo esc_url( $image_one ); ?>" alt="">
-                        <figcaption>
-
-                        </figcaption>
+                        <figcaption class="image-caption"><?php echo $image_one_caption; ?></figcaption>
                     </figure>
-                    <figure>
+                    <figure class="image--two">
                         <img src="<?php echo esc_url( $image_two ); ?>" alt="">
-                        <figcaption>
-
-                        </figcaption>
+                        <figcaption class="image-caption"><?php echo $image_two_caption; ?></figcaption>
                     </figure>
-                    <figure>
+                    <figure class="image--three">
                         <img src="<?php echo esc_url( $image_three ); ?>" alt="">
-                        <figcaption>
-
-                        </figcaption>
+                        <figcaption class="image-caption"><?php echo $image_three_caption; ?></figcaption>
                     </figure>
                 </div>
                 <div class="material_attribute">
+                    <?php
+                        $property_image         = get_field('property_image');
+                        $property_image_caption = get_field('property_image_caption');
+                    ?>
                     <?php the_field('text_box_two'); ?>
+
+                    <figure class="image--center">
+                        <img src="<?php echo esc_url( $property_image ); ?>" alt="">
+                        <figcaption class="image-caption"><?php echo $property_image_caption; ?></figcaption>
+                    </figure>
+                </div>
+
+
+                <div class="material_attribute">
+                    <?php the_field('text_box_three'); ?>
                 </div>
 
             </div>
