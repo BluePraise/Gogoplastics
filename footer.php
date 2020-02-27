cb090b93cb9603c914e66316476558b70d392dc1
MagalieLindaChetrit <magaliechetrit@gmail.com>
2020-02-27 13:28:08 +0100

    dotted titles and footer styling

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <footer>
 
     <div class="footer--col details">
-        <h4>Interested?</h4>
+        <h4 class="dotted-title">Interested?</h4>
+        <!-- <hr class="dotted"> -->
         <p>Please contact us at</p>
         <p>info@gogoplastics.com</p>
         <p>+31 6 24 52 43 30</p>
@@ -9,12 +10,12 @@
     </div>
     <div class="footer--col subscribe">
 
-        <h4>Subscribe newsletter</h4>
+        <h4 class="dotted-title">Subscribe newsletter</h4>
         <p><input class="newsletter" type="text"></p>
 
     </div>
     <div class="footer--col legal">
-        <h4>Disclaimer/Privacy policy</h4>
+        <h4 class="dotted-title">Disclaimer/Privacy policy</h4>
         <a href="https://instagram.com/magalielindamatters"><i data-feather="instagram"></i></a>
         <p></p>
     </div>
