451865093fdeac356ed0ba17ec20602afa3a719c
MagalieLindaChetrit <magaliechetrit@gmail.com>
2020-02-27 12:04:26 +0100

    responsive front-page

--- header.php
+++ header.php
@@ -10,8 +10,6 @@
         <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono&display=swap" rel="stylesheet">
         <link href="https://use.typekit.net/glz1kqk.css" rel="stylesheet">
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
-
-
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 
 		<?php wp_head(); ?>
@@ -21,6 +19,12 @@
 	<body <?php body_class(); ?>>
         <div class="wrapper">
         <!-- nav -->
-
+        <div class="h-wrapper">
+            <div id="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
         <?php gogo_nav(); ?>
 
